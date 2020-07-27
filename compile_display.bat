0a9fbf58686dedb35184811d128bac4d033f25fc
jnbarnesUdel <jnbarnes@udel.edu>
2020-07-27 14:38:33 -0400

    Delete compile_display.bat

--- compile_display.bat
+++
@@ -1,9 +0,0 @@
-GHDLloc\Ghdl\bin\ghdl.exe -a add.vhdl
-GHDLloc\Ghdl\bin\ghdl.exe -a display.vhdl
-GHDLloc\Ghdl\bin\ghdl.exe -a sub_add.vhdl
-GHDLloc\Ghdl\bin\ghdl.exe -a ALU.vhdl
-GHDLloc\Ghdl\bin\ghdl.exe -a adder_tb.vhdl
-GHDLloc\Ghdl\bin\ghdl.exe -a ALU_tb.vhdl
-GHDLloc\Ghdl\bin\ghdl.exe -a display_tb.vhdl
-GHDLloc\Ghdl\bin\ghdl.exe -e display_tb
-GHDLloc\Ghdl\bin\ghdl.exe -r display_tb --vcd=distb.vcd
