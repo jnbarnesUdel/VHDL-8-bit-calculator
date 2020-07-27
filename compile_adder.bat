8bd95ff4648484a359ca8c9cfeb784f0d5a469f0
jnbarnesUdel <jnbarnes@udel.edu>
2020-07-27 14:38:14 -0400

    Delete compile_adder.bat

--- compile_adder.bat
+++
@@ -1,7 +0,0 @@
-GHDLloc\Ghdl\bin\ghdl.exe -a add.vhdl
-GHDLloc\Ghdl\bin\ghdl.exe -a display.vhdl
-GHDLloc\Ghdl\bin\ghdl.exe -a sub_add.vhdl
-GHDLloc\Ghdl\bin\ghdl.exe -a ALU.vhdl
-GHDLloc\Ghdl\bin\ghdl.exe -a adder_tb.vhdl
-GHDLloc\Ghdl\bin\ghdl.exe -e Adder_tb
-GHDLloc\Ghdl\bin\ghdl.exe -r Adder_tb --vcd=addertb.vcd
